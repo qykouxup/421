3442870c64b45eccf926a8f07a7f03dc8faebd26
toto <toto@toto.com>
2016-03-28 14:55:06 +0200

    Second version of 421
    Doubles & Rows added

--- cequetuveux.py
+++ cequetuveux.py
@@ -18,11 +18,29 @@ def roll_dice():
 	if winner == False:
 		winner = check_421(dices)
 	if winner == False:
+		winner = check_double(dices)
+	if winner == False:
+		winner = check_row(dices)
+	if winner == False:
 		print 'Try again'
-	
+
+def check_double(dices):
+	if dices[0] == dices[1] == 1:
+		print ' You win %s points with a double ace' % dices[2]
+		return True
+	else:
+		return False
+
 def check_brelan(dices):
 	if dices in dict_brelan:
-		print 'You win %s points' % dict_brelan[dices]
+		print 'You win %s points with a brelan' % dict_brelan[dices]
+		return True
+	else:
+		return False
+
+def check_row(dices):
+	if ((dices[0]  + 1) == dices [1]) and ((dices[1]  + 1) == dices [2]):
+		print 'You make a row ! It gives you 2 points'
 		return True
 	else:
 		return False
